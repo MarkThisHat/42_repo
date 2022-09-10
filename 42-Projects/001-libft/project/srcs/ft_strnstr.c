e70688819a3fd879ce9368e063f612d5923cb75f
Marcos Holmes <marcos.chiu@gmail.com>
2022-09-09 22:12:59 -0300

    strnstr

--- 42-Projects/001-libft/project/srcs/ft_strnstr.c
+++ 42-Projects/001-libft/project/srcs/ft_strnstr.c
@@ -1,31 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/07 17:26:35 by maalexan          #+#    #+#             */
-/*   Updated: 2022/09/07 17:26:35 by maalexan         ###   ########.fr       */
+/*   Created: 2022/09/07 21:17:05 by maalexan          #+#    #+#             */
+/*   Updated: 2022/09/07 21:17:05 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-# include <unistd.h>
+#include "libft.h"
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_isdigit(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char	*big, const char *little, size_t len)
-char	*ft_strchr(const char *str, int c);
-char	*ft_strrchr(const char *str, int c);
-size_t	ft_strlen(const char *str);
+{
+	size_t	i;
+	size_t	len_l;
+	size_t	len_b;
 
-#endif
+	i = 0;
+	if (len == 0)
+		return (0);
+	len_l = ft_strlen(little);
+	len_b = ft_strlen(big);
+	if (len_l == 0)
+		return ((char *)&big[i]);
+	while ((i <= len_b - len_l) && (i <= len - len_l))
+	{
+		if (big[i] == little[0])
+			if (ft_strncmp(&big[i], &little[0], len_l) == 0)
+				return ((char *)&big[i]);
+		i++;
+	}
+	return (0);
+}
