b55259544af58d3fe2ecd3ed6cbb4dd7892e6b1b
Markthishat <marcos.mariano.cef@gmail.com>
2023-01-25 13:24:16 -0300

    might be reading size correctly

--- 42-Projects/005-fdf/incl/fdf.h
+++ 42-Projects/005-fdf/incl/fdf.h
@@ -6,11 +6,41 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 12:39:59 by maalexan          #+#    #+#             */
-/*   Updated: 2023/01/22 12:41:51 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/01/25 12:25:24 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libs/libft/incl/libft.h"
+#ifndef FDF_H
+#define FDF_H
 
-int	validate_usage(int	argc, char **argv);
+# include "../libs/libft/incl/libft.h"
+# include <fcntl.h>
+# include <math.h>
+
+typedef struct	s_img {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_img;
+
+typedef struct	s_mlxs {
+	void	*mlx;
+	void	*win;
+	t_img	*img1;
+	t_img	*img2;
+	int		col;
+	int		row;
+	int		x;
+	int		y;
+	int		z;
+	int		color;
+}			t_mlxs;
+
+
+int		count_map(int fd, t_mlxs *ms);
+int		validate_usage(int argc, char **argv, t_mlxs *ms);
 void	leave_program(char *str, int fd, int return_code);
+
+#endif
--- 42-Projects/005-fdf/srcs/fdf.c
+++ 42-Projects/005-fdf/srcs/fdf.c
@@ -6,20 +6,46 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 11:37:28 by maalexan          #+#    #+#             */
-/*   Updated: 2023/01/22 13:32:14 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:23:07 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/fdf.h"
 
+/*
+typedef struct	s_img {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_img;
+
+typedef struct	s_mlxs {
+	void	*mlx;
+	void	*win;
+	t_img	*img1;
+	t_img	*img2;
+	int		col;
+	int		row;
+	int		x;
+	int		y;
+	int		z;
+	int		color;
+}			t_mlxs;
+*/
+
 int	main(int argc, char **argv)
 {
-	if (validate_usage(argc, argv))
+	t_mlxs	main_struct;
+
+	if (validate_usage(argc, argv, &main_struct))
 		leave_program(0, 0, 0);
 }
 
-int	validate_usage(int	argc, char **argv)
+int	validate_usage(int	argc, char **argv, t_mlxs *ms)
 {
+	int	fd;
 	int	len;
 
 	if (argc != 2)
@@ -27,7 +53,47 @@ int	validate_usage(int	argc, char **argv)
 	len = ft_strlen(argv[1]);
 	if (len < 5 || ft_strncmp(".fdf", &argv[1][len -4], 5))
 		leave_program("Please input a valid <.fdf> file\n", 2, 1);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		leave_program("Error opening file\n", 2, 1);
+	if (!count_map(fd, ms))
+		leave_program("Can't parse this map\n", 2, 1);
+	ft_printf("Map %s has %i rows and %i columns\n", argv[1], ms->row, ms->col);
+	close(fd);
 	return (1);
+}
+
+int	count_map(int fd, t_mlxs *ms)
+{
+	char	c;
+	char	col_val;
+
+	c = ' ';
+	while (c == ' ')
+		read(fd, &c, 1);
+	ms->row = 0;
+	ms->col = 1;
+	col_val = 1;
+	while(read(fd, &c, 1))
+	{
+		if (c == ' ' && col_val)
+		{
+			while (c == ' ')
+				read(fd, &c, 1);
+			if (c != '\n')
+				ms->col++;
+		}
+		if (c == '\n' || c == 0)
+		{
+			ms->row++;
+			col_val = 0;
+			while (read(fd, &c, 1) && (c == '\n' || c == 0))
+				read(fd, &c, 1);
+		}
+	}
+	if (ms->row)
+		return (1);
+	return (0);
 }
 
 void	leave_program(char *str, int fd, int return_code)
