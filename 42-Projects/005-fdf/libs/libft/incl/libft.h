9d1b3617c47e4845674bfe3aae0712612d02b162
Marcos Alexandre Chiu Mariano Da Silva <maalexan@c1r7p2.42sp.org.br>
2023-01-26 17:15:34 -0300

    ft_atoi_Base

--- 42-Projects/005-fdf/libs/libft/incl/libft.h
+++ 42-Projects/005-fdf/libs/libft/incl/libft.h
@@ -43,6 +43,7 @@ int		ft_isdigit(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_atoi(const char *nptr);
+int		ft_atoi_base(const char *str, int base);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_itoa(int n);
