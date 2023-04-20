1c5807b92d54f40a7c650c6ef8a57c03a4ee2217
Markthishat <marcos.mariano.cef@gmail.com>
2023-04-20 18:43:31 -0300

    structing

--- 42-Projects/007-pushswap/srcs/push_swap.c
+++ 42-Projects/007-pushswap/srcs/push_swap.c
@@ -12,6 +12,45 @@
 
 #include "push_swap.h"
 
+void	print_pile(t_pile *pile)
+{
+	while (pile->next)
+	{
+		ft_printf("%i", pile->n);
+		print_pile(pile->next);
+	}
+}
+
+void	fill_a(t_pile *a, int len, char **nbrs)
+{
+	static int	i;
+
+	i++;
+	while (i < len)
+	{
+		a->next = malloc(sizeof(t_pile));
+		if (!a->next)
+			ft_printf("bummer on i %i\n", i);
+		a->n = ft_atoi(nbrs[i]);
+		fill_a(a->next, len, nbrs);
+	}
+	a->next = NULL;
+}
+
+t_pile	*stack_up(int argc, char **argv)
+{
+	t_pile *a;
+
+	if (argc == 2)
+		exit(0);
+	a = malloc(sizeof(t_pile) * argc);
+	if (!a)
+		exit(4);
+	a->prev = NULL;
+	fill_a(a, argc, argv);
+	return (a);
+}
+
 int	main(int argc, char **argv)
 {
 	t_pile	*a;
@@ -19,8 +58,9 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		return (1);
+	a = stack_up(argc, argv);
 	check_args(argc, argv);
 	ft_printf("nice args!\n");
-	(void)a;
+	print_pile(a);
 	(void)b;
 }
