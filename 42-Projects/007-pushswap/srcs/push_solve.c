4eda35df860d5329859c191fa223df0b5de16710
Markthishat <marcos.mariano.cef@gmail.com>
2023-04-28 18:36:56 -0300

    working on something

--- 42-Projects/007-pushswap/srcs/push_solve.c
+++ 42-Projects/007-pushswap/srcs/push_solve.c
@@ -34,8 +34,8 @@ static int measure_index(t_item *pile, int size, int pivot)
 	return (total_distance /size);
 }
 
-
-static int	split_stack(t_ctrl *c, int pivot)
+/*
+static int	split_rotate(t_ctrl *c, int pivot)
 {
 	if (c->size_b < 2)
 	{
@@ -51,21 +51,62 @@ static int	split_stack(t_ctrl *c, int pivot)
 	else
 		return (rotate_a(c));
 }
+*/
+
+static int	split_swap(t_ctrl *c, int pivot)
+{
+	if (c->size_b < 2)
+	{
+		if	(c->size_a > 1 && c->head_a->i < c->head_a->next->i)
+			return(swap_a(c));
+		else if (c->head_a->i < pivot)
+			return (push_b(c));
+		else
+			return (rotate_a(c));
+	}
+	if (c->head_a->i < pivot)
+		return (push_b(c));
+	else if (c->head_b->i < c->head_b->next->i)
+		return (rotate_both(c));
+	else
+		return (rotate_a(c));
+}
 
 int	sort_stack(t_ctrl *c, int quadrant, int size)
 {
 	int	pivot;
+	int len;
+	int	i;
 
-	ft_printf("Idx A %i\n", measure_index(c->head_a, c->size_a, 0));
-	(void)quadrant;
+	i = 0;
+	len = size * 2;
 	pivot = size / 2;
-	ft_printf("Final pivot: %i and size %i\n", pivot, size);
-	while (c->size_b < pivot)
+	ft_printf("Quadrant: %i\nPivot: %i\n", quadrant, pivot);
+	while (len--)
 	{
-		split_stack(c, pivot);
-		print_full_stacks(c);
+		split_swap(c, pivot);
+		i++;
 	}
+	print_full_stacks(c);
 	ft_printf("Idx A %i\n", measure_index(c->head_a, c->size_a, pivot));
 	ft_printf("Idx B %i\n", measure_index(c->head_b, c->size_b, 0));
+	ft_printf("%i moves\n", i);
 	return (pivot);
 }
+
+/*
+int	sort_four(t_item *head, t_item *tail)
+{
+	int	i;
+	int	moves;
+	int	ext[4];
+	int	lowest;
+
+	i = 0;
+	moves = 0;
+	ext[0] = head->i;
+	ext[1] = head->next->i;
+	ext[2] = tail->prev->i;
+	ext[3] = tail->i;
+}
+*/
