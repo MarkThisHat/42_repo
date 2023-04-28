12ab0ee0e8c8c0fdb85e97aa50c6693ec2e3d10e
Markthishat <marcos.mariano.cef@gmail.com>
2023-04-28 15:04:14 -0300

    prototype one

--- 42-Projects/007-pushswap/srcs/push_solve.c
+++ 42-Projects/007-pushswap/srcs/push_solve.c
@@ -1,5 +1,40 @@
 #include "push_swap.h"
 
+static int measure_index(t_item *pile, int size, int pivot)
+{
+	int *indices;
+	int i;
+	int total_distance;
+	int	ideal_position;
+	int	distance;
+
+	indices = ft_calloc(size, sizeof(int));
+	if (!indices)
+		return (-1);
+	i = 0;
+	total_distance = 0;
+	while (pile)
+	{
+		indices[i] = pile->i;
+		pile = pile->next;
+		i++;
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (pivot)
+			ideal_position = indices[i] - pivot; // Ideal position in the sorted pile
+		else
+			ideal_position = size -1 - indices[i];
+		distance = ideal_position - i; // Distance from the ideal position
+		if (distance < 0)
+			distance = -distance;
+		total_distance += distance;
+	}
+	free(indices);
+	return (total_distance /size);
+}
+
+
 static int	split_stack(t_ctrl *c, int pivot)
 {
 	if (c->size_b < 2)
@@ -21,12 +56,16 @@ int	sort_stack(t_ctrl *c, int quadrant, int size)
 {
 	int	pivot;
 
-	pivot = (size * quadrant) / 4;
+	ft_printf("Idx A %i\n", measure_index(c->head_a, c->size_a, 0));
+	(void)quadrant;
+	pivot = size / 2;
 	ft_printf("Final pivot: %i and size %i\n", pivot, size);
-	while (c->size_b < size / 2)
+	while (c->size_b < pivot)
 	{
-		split_stack(c, size / 2);
-		print_stacks(c);
+		split_stack(c, pivot);
+		print_full_stacks(c);
 	}
+	ft_printf("Idx A %i\n", measure_index(c->head_a, c->size_a, pivot));
+	ft_printf("Idx B %i\n", measure_index(c->head_b, c->size_b, 0));
 	return (pivot);
 }
