2ffec38ee52a9e7ed5f4b5ef2d9e20e9b928ada5
Markthishat <marcos.mariano.cef@gmail.com>
2023-05-08 11:49:31 -0300

    better optimizer

--- 42-Projects/007-pushswap/srcs/opt_swap.c
+++ 42-Projects/007-pushswap/srcs/opt_swap.c
@@ -21,6 +21,103 @@ static int is_redundant(int move1, int move2)
 	return (0);
 }
 
+static	int	change_move(int move, int upgrade)
+{
+	if (upgrade && move == SA)
+		return (SS);
+	if (upgrade && move == SB)
+		return (SS);
+	if (upgrade && move == RA)
+		return (RR);
+	if (upgrade && move == RB)
+		return (RR);
+	if (upgrade && move == RRA)
+		return (RRR);
+	if (upgrade && move == RRB)
+		return (RRR);
+	if (!upgrade && move == SA)
+		return (SB);
+	if (!upgrade && move == SB)
+		return (SA);
+	if (!upgrade && move == RA)
+		return (RB);
+	if (!upgrade && move == RB)
+		return (RA);
+	if (!upgrade && move == RRA)
+		return (RRB);
+	if (!upgrade && move == RRB)
+		return (RRA);
+	return (0);
+}
+
+static t_sol	*find_replacement(t_sol *navi, int move)
+{
+	if (!move)
+		return (NULL);
+	while (navi && navi->move >= PA)
+	{
+		if (navi->move == move)
+			return (navi);
+		else
+			navi = navi->next;
+	}
+	return (NULL);
+}
+
+static int wipe_unecessary(t_sol *navi, t_sol *head, int optimized)
+{
+	t_sol *temp;
+	t_sol *prev;
+
+	temp = NULL;
+	prev = NULL;
+	while (navi)
+	{
+		if (navi->next && is_redundant(navi->move, navi->next->move))
+		{
+			temp = navi->next->next;
+			remove_node(head, navi->next);
+			remove_node(head, navi);
+			if (prev)
+				prev->next = temp;
+			navi = temp;
+			optimized = 0;
+		}
+		else
+		{
+			prev = navi;
+			navi = navi->next;
+		}
+	}
+	return (optimized);
+}
+
+void optimize_solution(t_sol *navi, t_sol *head)
+{
+	t_sol	*temp;
+	int		opt;
+
+	opt = 0;
+	while (!opt)
+		opt = wipe_unecessary(navi, head, 1);
+	while (navi && navi->move <= PA)
+		navi = navi->next;
+	if (!navi)
+		return;
+	temp = NULL;
+	if (navi->move >= SA && navi->move <= RRB)
+	{
+		temp = find_replacement(navi, change_move(navi->move, 0));
+		if (temp)
+		{
+			navi->move = change_move(navi->move, 1);
+			remove_node(head, temp);
+		}
+	}
+	optimize_solution(navi->next, head);
+}
+
+/*
 static	int	compli_move(int move)
 {
 	if (move == SA)
@@ -53,46 +150,4 @@ static	int	upgrade_move(int move)
 	if (move == RRB)
 		return (RRR);
 	return (0);
-}
-
-static t_sol	*find_replacement(t_sol *navi, int move)
-{
-	if (!move)
-		return (NULL);
-	while (navi && navi->move >= PA)
-	{
-		if (navi->move == move)
-			return (navi);
-		else
-			navi = navi->next;
-	}
-	return (NULL);
-}
-
-void optimize_solution(t_sol *navi, t_sol *head)
-{
-	t_sol *temp;
-
-	temp = NULL;
-/*	while (navi && navi->move <= PA)
-		navi = navi->next;*/
-	if (!navi)
-		return;
-	if (navi->next && is_redundant(navi->move, navi->next->move))
-	{
-		temp = navi->next;
-		remove_node(head, navi);
-		remove_node(head, temp);
-		optimize_solution(temp, head);
-	}
-	else if (navi->move >= SA && navi->move <= RRB)
-	{
-		temp = find_replacement(navi, compli_move(navi->move));
-		if (temp)
-		{
-			navi->move = upgrade_move(navi->move);
-			remove_node(head, temp);
-		}
-	}
-	optimize_solution(navi->next, head);
-}
+}*/
