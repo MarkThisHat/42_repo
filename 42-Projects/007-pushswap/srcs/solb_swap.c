09cd7559afada94f8ed9ae73eda64d48b3921851
Markthishat <marcos.mariano.cef@gmail.com>
2023-05-10 16:56:04 -0300

    may be solved

--- 42-Projects/007-pushswap/srcs/solb_swap.c
+++ 42-Projects/007-pushswap/srcs/solb_swap.c
@@ -12,6 +12,39 @@ int	find_climb_chunk(int size)//, int pivot)
 //	(void)pivot;
 }
 
+int	take_in_b(t_ctrl *c, int aim)
+{
+	static int	bigger;
+	static int	smaller;
+ft_printf("Aim: %i\nBigger: %i\nSmaller: %i\n", aim, bigger, smaller);
+	if (c->size_b < 3)
+		return (0);
+	if (!bigger && !smaller)
+	{
+		bigger = c->head_b->i;
+		smaller = c->head_b->i;
+	}
+	if (c->size_b == 3)
+		sort_three(c, c->head_b, 1);
+
+	if (aim > bigger)
+		bigger = aim;
+	if (aim < smaller)
+		smaller = aim;
+	if (aim == bigger || aim == smaller)
+		c->stream = log_move(push_b(c), c->stream, c);
+	if (aim == smaller)
+		c->stream = log_move(rotate_b(c), c->stream, c);
+	if (aim == bigger || aim == smaller)
+		return (1);
+	while (c->head_b->i > aim)
+		c->stream = log_move(rotate_b(c), c->stream, c);
+	c->stream = log_move(push_b(c), c->stream, c);
+	while (c->head_b->i != bigger)
+		c->stream = log_move(rev_rotate_b(c), c->stream, c);
+	return (1);
+}
+
 void	pogo_sol(t_ctrl *c, int size)
 {
 	int	max;
@@ -26,8 +59,8 @@ void	pogo_sol(t_ctrl *c, int size)
 		c->answer = first_move_big(c, RA);
 	c->stream = get_stream(c->answer);
 	print_full_stacks(c);
-	ft_printf("First pivot and max are %i and %i\n", pivot, max);
-	while (c->size_a > 3 && max)
+//	ft_printf("First pivot and max are %i and %i\n", pivot, max);
+	while (c->size_a)// > 3 && max)
 	{
 		aim = has_amidst(c->head_a, pivot, max);
 		if (aim >= 0)
@@ -38,8 +71,8 @@ void	pogo_sol(t_ctrl *c, int size)
 			else
 				while (c->head_a->i != aim)
 					c->stream = log_move(rev_rotate_a(c), c->stream, c);
-			//prepare b target
-			c->stream = log_move(push_b(c), c->stream, c);
+			if (!take_in_b(c, aim))
+				c->stream = log_move(push_b(c), c->stream, c);
 		}
 		else
 		{
@@ -48,5 +81,7 @@ void	pogo_sol(t_ctrl *c, int size)
 			ft_printf("Pivot and max are %i and %i\n", pivot, max);
 		}
 	}
-			print_full_stacks(c);
+	while (c->size_b)
+		c->stream = log_move(push_a(c), c->stream, c);
+	print_full_stacks(c);
 }
