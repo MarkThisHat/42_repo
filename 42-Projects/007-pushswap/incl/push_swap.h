91a1329cf88ff8f095990e4150a09f90c12df91f
Markthishat <marcos.mariano.cef@gmail.com>
2023-05-13 10:45:40 -0300

    header norm

--- 42-Projects/007-pushswap/incl/push_swap.h
+++ 42-Projects/007-pushswap/incl/push_swap.h
@@ -79,7 +79,7 @@ int		push_a(t_ctrl *c);
 int		swap_both(t_ctrl *c);
 int		rotate_both(t_ctrl *c);
 int		rev_rotate_both(t_ctrl *c);
-//		merge_swap.c (4)
+//		merge_swap.c
 void	merge_sort(int *stack, int *invers, int left, int right);
 //		move_swap.c
 int		do_move(t_ctrl *c, int move);
@@ -93,19 +93,19 @@ void	switch_adj_nodes(t_item *a, t_item *b);
 void	remove_node(t_sol *head, t_sol *node);
 t_item	*sever_node(t_item *item);
 t_item	*place_node(t_item *item, t_item *edge);
-//		opt_swap.c(4)
+//		opt_swap.c
 void	optimize_solution(t_sol *navi, t_sol *head);
-//		print_swap.c (2)
+//		print_swap.c
 void	print_sol(t_sol *s);
 //		ps_utils.c
 void	leave_program(char *str, int return_code);
 void	empty_stack(t_item *item, int return_code);
 void	free_and_leave(t_ctrl *c, int return_code);
 void	set_control(t_ctrl *c, t_item *item, int size);
-//		push_check.c (4)
+//		push_check.c
 int		is_sorted(t_ctrl *c);
 void	check_args(int argc, char **argv);
-//		push_index(5)
+//		push_index
 void	assess_pile(t_item *head, int size);
 //		push_sol.c
 t_sol	*prep_sol(t_sol *s);
@@ -116,41 +116,12 @@ t_sol	*first_move_big(t_ctrl *c, int move);
 void	a_is_three(t_ctrl *c, t_sol *sol);
 void	small_sol(t_ctrl *c, int size);
 void	sort_three(t_ctrl *c, t_item *head, int is_b);
-//		solc_swap.c (5)
+//		solc_swap.c
 void	cheap_sol(t_ctrl *c, int size);
-//		solc_utils.c (5)
+//		solc_utils.c
 int		cal_cost(t_item *package, t_item *oppo_head, int *moves);
 int		find_cushy_spot(int item, t_item *op);
-//		solve_swap.c(4)
+//		solve_swap.c
 void	find_sol(t_ctrl *c);
 
-/*
-//		big_swap.c
-void	big_sol(t_ctrl *c, int size);
-
-//		push_reset.c(2)
-void	reset_stacks(t_ctrl *c, t_sol *solution);
-//		zwap_pivot.c (4)
-int		find_pivot(int *arr, int len);
-
-//		TODO
-void	set_array(int *sol, int size);
-void	solve_small(t_ctrl *c, int *sol);
-void	array_sol(t_sol *s, int *sol, t_ctrl *c);
-int		find_target(t_item *stack, int target);
-t_sol	*prep_sol(t_sol *s);
-t_sol	*trace_move(int move, t_sol *stack, t_ctrl *c);
-t_sol	*apply_sol(t_sol *s, t_ctrl *c);
-
-//unconsolidated
-int		sort_stack(t_ctrl *c, int quadrant, int size);
-void	pogo_sol(t_ctrl *c, int size);
-void	sol_c(t_ctrl *c, int size);
-
-//not project
-void	print_stacks(t_ctrl *c);
-void	print_full_stacks(t_ctrl *c);
-void	test_moves(t_ctrl *c, int mode);
-void	print_sol_detailed(t_sol *s);
-*/
 #endif
